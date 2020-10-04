092d8f83062590da9a561052a2e2e795a29e1bd0
julioalcantara <alcantara.juliosousa@gmail.com>
2020-10-04 13:37:43 +0100

    added side bar navigation

--- shared.js
+++ shared.js
@@ -2,21 +2,36 @@ const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const modalNoButton = document.querySelector('.modal__action--negative');
 const selectPlanButtons = document.querySelectorAll('.plan button');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
 //console.dir(backdrop)
 
 for (let i = 0; i < selectPlanButtons.length; i++){ 
     selectPlanButtons[i].addEventListener('click', function(){
-        backdrop.style.display = 'block';
-        modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
 modalNoButton.addEventListener('click', closeModal);
 
+toggleButton.addEventListener('click', function(){
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
 
 function closeModal(){
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
 }
+
